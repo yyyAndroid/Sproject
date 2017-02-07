94458a25feb2b59cc8e0e18c2afd4c1c34dbb50b
yyyAndroid <noExceptionYes@gmail.com>
2017-02-07 10:34:13 +0800

    添加viewpage

--- app/src/main/java/com/abe/dwwd/sporjectone/ui/activity/ViewPageActivity.java
+++ app/src/main/java/com/abe/dwwd/sporjectone/ui/activity/ViewPageActivity.java
@@ -3,10 +3,12 @@ package com.abe.dwwd.sporjectone.ui.activity;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.abe.dwwd.sporjectone.R;
 import com.abe.dwwd.sporjectone.ui.adapter.ZoomOutPageTransformer;
@@ -29,8 +31,8 @@ public class ViewPageActivity extends BaseActivity {
 
     private void initViewPage(){
 
-       int pagerWidth = (int) (getResources().getDisplayMetrics().widthPixels * 8.5f / 10.0f);
-        viewPager.measure(0, 0);
+       int pagerWidth = (int) (getResources().getDisplayMetrics().widthPixels * 6.5f / 10.0f);
+//        viewPager.measure(0, 0);
         ViewGroup.LayoutParams lp = viewPager.getLayoutParams();
         if (lp == null) {
             lp = new ViewGroup.LayoutParams(pagerWidth, LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -38,9 +40,37 @@ public class ViewPageActivity extends BaseActivity {
             lp.width = pagerWidth;
         }
         viewPager.setLayoutParams(lp);
-        viewPager.setPageMargin(20);
-        viewPager.setCurrentItem(4);
-        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        viewPager.setPageMargin(10);
+//        viewPager.setOffscreenPageLimit(4);
+//      viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+
+
+
+/*        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//                Log.d(TAG, "onPageScrolled: =" + position);
+                if (viewPager.getParent() != null) {
+                    ((RelativeLayout)viewPager.getParent()).invalidate();  //更新超出区域页面，否则会出现页面缓存，导致页面效果不佳
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+               *//* Log.d(TAG, "onPageSelected: =" + position + "  =  " + agoPos);
+                if (markerList.size() == 0)
+                    return;
+                Marker marker = markerList.get(position);
+                Marker agoMarker = markerList.get(agoPos);
+                mMarkerUtil.setDefMarker(agoMarker);
+                mMarkerUtil.setBigMarker(marker);
+                agoPos = position;*//*
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });*/
         viewPager.setAdapter(new PagerAdapter() {
 
             @Override
@@ -70,7 +100,8 @@ public class ViewPageActivity extends BaseActivity {
         for (int imgid : mImageids){
             ImageView imageView = new ImageView(getApplicationContext());
             imageView.setImageResource(imgid);
-            imageView.setLayoutParams(new ViewGroup.LayoutParams(50,50));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(30, ViewGroup.LayoutParams.MATCH_PARENT));
             imgs.add(imageView);
         }
     }
